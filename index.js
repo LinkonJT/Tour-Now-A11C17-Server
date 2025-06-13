4f2a1c3eda29ba3025aa2ffbca62cfa270722b08
LinkonJT <linkontripura@gmail.com>
2025-06-13 17:05:48 +0600

    installed all the necessary tools

---
+++ index.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+//middlewares
+app.use(express.json());
+app.use(cors())
+
+
+/*******########### Start:  MongoDB  ###########*****/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3h4lqut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    /***********************start: playing field******************************************* */
+
+const tourPackagesCollection = client.db('tourNowDB').collection('all-packages')
+
+/***### post/insert in the MongoDB ### */
+app.post('/all-packages', async (req, res)=>{
+  const newPackage = req.body
+  const result =await tourPackagesCollection.insertOne(newPackage);
+  res.send (result)
+})
+ /**##### END: post/insert in the MongoDB ####*/
+
+app.get('/all-packages', async (req, res)=>{
+  const packages = await tourPackagesCollection.find().toArray()
+  res.send (packages)
+})
+
+
+
+    /***********************end: playing field******* ******************************************/
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+/*******############ END:  MongoDB  #############*****/
+
+
+
+/*********######ExpressJS ########## */
+app.get('/', (req, res) => {
+  res.send('TourNOW Data base running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
